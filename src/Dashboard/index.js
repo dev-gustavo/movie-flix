6ea68a863d46aef1d4071015760036180f2187e6
Gustavo Maciel <gustavomaciel@Gustavos-MacBook-Air.local>
2022-07-28 23:00:24 +0100

    DashboardScreesFailed

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,16 @@
 
 import '../Dashboard/styleDashboard.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UncontrolledExample from './UncontrolledExample';
+import movieApi from '../UrlAPI/movieApi'
 
 function Dashboard(){
     return(
+        
         <div className="dashbord">
+            
+            <UncontrolledExample/>
+
             <div className='boxA'> 
                 <h1> TOP RATED </h1>
             </div>
